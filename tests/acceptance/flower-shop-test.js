789ea905ee4b530c4847b0edf66194504ae27365
Kyle Weintraub <weintraub.kyle@gmail.com>
2024-07-23 14:04:54 -0600

    components, yield, and outlet

--- tests/acceptance/flower-shop-test.js
+++ tests/acceptance/flower-shop-test.js
@@ -9,6 +9,8 @@ module('Acceptance | flower shop', function (hooks) {
     await visit('/');
 
     assert.strictEqual(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('FlowerShop');
     assert.dom('h2').hasText('Welcome to Flower Shop!');
 
     assert.dom('.jumbo a.button').hasText('About Us');
@@ -21,6 +23,8 @@ module('Acceptance | flower shop', function (hooks) {
     await visit('/about');
 
     assert.strictEqual(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('FlowerShop');
     assert.dom('h2').hasText('About Flower Shop');
 
     assert.dom('.jumbo a.button').hasText('Contact Us');
@@ -33,11 +37,31 @@ module('Acceptance | flower shop', function (hooks) {
     await visit('/getting-in-touch');
 
     assert.strictEqual(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('FlowerShop');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('.jumbo a.button').hasText('About');
     await click('.jumbo a.button');
 
     assert.strictEqual(currentURL(), '/about');
+  });
+
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('FlowerShop');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.strictEqual(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.strictEqual(currentURL(), '/');
   });
 });
